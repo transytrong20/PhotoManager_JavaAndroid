28bafc8ff4b2a07eb3aec18168f68133e06340b8
trongts <transytrong20@gmail.com>
2025-04-13 17:19:06 +0700

    .

--- app/src/main/java/com/example/photomanager/screens/detail/PhotoDetailScreen.kt
+++ app/src/main/java/com/example/photomanager/screens/detail/PhotoDetailScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
